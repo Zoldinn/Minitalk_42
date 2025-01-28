201c94c1d5b69991a813ce66ee4ffeb87d2de844
lefoffan - leon <lefoffan@student.42perpignan.fr>
2025-01-28 11:59:09 +0100

    did a proto function for sending signals which represent bits of str

--- client.c
+++ client.c
@@ -6,19 +6,42 @@
 /*   By: lefoffan <lefoffan@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/27 14:18:02 by lefoffan          #+#    #+#             */
-/*   Updated: 2025/01/27 18:17:03 by lefoffan         ###   ########.fr       */
+/*   Updated: 2025/01/28 11:47:44 by lefoffan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
+void	send_sig(int pid, char *str)
+{
+	int		bit;
+	int		i;
+
+	i = 7;
+	while (str);
+	{
+		while (i >= 0)
+		{
+			bit = (*str >> i) & 1;
+			if (bit)
+				kill(pid, SIGUSR1);
+			else
+				kill(pid, SIGUSR2);
+			--i;
+			usleep(100);
+		}
+		str++;
+	}
+}
+
 int	main(int ac, char **av)
 {
+
 	if (ac != 3)
 	{
 		ft_putstr_fd(2, "Enter : Server PID and message\n");
 		exit(EXIT_FAILURE);
 	}
-	
+	send_sig(ft_atoi(av[1]), av[2]);
 	exit(EXIT_SUCCESS);
 }
--- server.c
+++ server.c
@@ -6,25 +6,16 @@
 /*   By: lefoffan <lefoffan@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/27 14:17:53 by lefoffan          #+#    #+#             */
-/*   Updated: 2025/01/27 17:11:32 by lefoffan         ###   ########.fr       */
+/*   Updated: 2025/01/28 11:51:49 by lefoffan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-int	get_sig(int signal)
-{
-	if (signal == SIGUSR1)
-		return (0);
-	else if (signal == SIGUSR2)
-		return (1);
-	return (-1);
-}
-
-
-
 int main(void)
 {
 	ft_printf("server PID : %d\n", getpid());
+	while (1)
+		pause();
 	exit(EXIT_SUCCESS)
 }
